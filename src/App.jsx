5c19de4abfa927473efdd66d16ea625c6be567cd
Brittany Dugger <brittanydugger@Brittanys-MBP.attlocal.net>
2024-02-19 15:12:30 -0600

    completed select a featured puppy

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,27 @@ import {puppyList} from './data.js'
 
 function App() {
 const [puppies, setPuppies] = useState(puppyList)
+const [featPupId, setFeatpupId] =  useState(null)
 // console.log(puppies)
+const featuredPup = puppies.find((pup) => pup.id === featPupId)
+console.log(featuredPup)
 
   return (
     <>
       <div className='App'>
+      
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return <p onClick={() => {setFeatpupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
         })}
+       {featPupId && 
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Age: {featuredPup.email}</li>
+          </ul>
+        </div>
+        }
       </div>
   
     </>
